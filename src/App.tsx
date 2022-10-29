ef8d96c1700b97ac5513b1184c90ac058b550f7d
Axel Friberg <hello@axelfriberg.com>
2022-10-29 13:23:40 +0200

    Add click counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import "./App.css";
+import { useState } from "react";
 import { Button } from "./Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleIncrementCount = () => setCount((count) => count + 1);
+
   return (
     <div className="App">
       <h1>Axel's Fancy Buttons</h1>
+      <p>Count is: {count}</p>
       <div className="button-container">
-        <Button>Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-        <Button startIcon={<FontAwesomeIcon icon={faEnvelope} />}>
+        <Button onClick={handleIncrementCount}>Contained</Button>
+        <Button variant="outlined" onClick={handleIncrementCount}>
+          Outlined
+        </Button>
+        <Button
+          startIcon={<FontAwesomeIcon icon={faEnvelope} />}
+          onClick={handleIncrementCount}
+        >
           Start Icon
         </Button>
-        <Button variant="outlined" endIcon={<FontAwesomeIcon icon={faSave} />}>
+        <Button
+          variant="outlined"
+          endIcon={<FontAwesomeIcon icon={faSave} />}
+          onClick={handleIncrementCount}
+        >
           End Icon
         </Button>
       </div>
